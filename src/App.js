d8436cc45034529c591d324aa0e4607bd07aab9a
Myreage <remy.vandyk@free.fr>
2021-01-17 16:43:08 +0100

    feat: Add simple router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Header from "./components/Header/Header"
 import Members from "./components/Members/Members"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>  
+      <Router>
+      <Navbar/>    
+          
       <div className="Content">
-        <Header/> 
-        <Members/> 
+        <Switch>
+          <Route path="/guild">
+            <Header/> 
+            <Members/> 
+          </Route>
+          <Route path="/raid">
+            <h1>RAID</h1>
+          </Route>
+          <Route path="/mythic">
+            <h1>MYTHIC +</h1>
+          </Route>
+          <Route path="/settings">
+            <h1>GESTION</h1>
+          </Route>
+        </Switch>
       </div>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,29 @@
 import './Navbar.css';
 import settings from "./static/settings.svg"
+import {
+  Link
+} from "react-router-dom";
 
 export default function Navbar() {
-    return (
+    return (      
         <div id="Navbar">
           <div id="Player">
             <h1 id="Pseudo">Incelprince</h1>
             <p id="RioScore"><span class="yellow">9807</span> rio</p>  
           </div>  
           <ul>
-            <li><a class="active" id="guild" href="">GUILDE</a></li>
-            <li><a id="raid" href="">RAID</a></li>
-            <li><a id="mythic" href="">MYTHIC+</a></li>
+            <li><Link class="active" id="guild" to="/guild">GUILDE</Link></li>
+            <li><Link id="raid" to="/raid">RAID</Link></li>
+            <li><Link id="mythic" to="/mythic">MYTHIC+</Link></li>
           </ul>
-          <div id="settings">
+          <Link id="settings" to="/settings">
             <img
               src={settings}
               style={{filter: "invert(100%) sepia(100%) saturate(0%) hue-rotate(182deg) brightness(104%) contrast(102%)"}}
               height="40vh" /> 
               <p>Gestion</p>
-          </div>
-        </div>  
+          </Link>
+        </div>
     );
   }
   
